e05ab5bfc5586869aa460f0eca881f07f72fb4e3
german <heinz.german@gmail.com>
2020-05-15 11:53:11 +0200

    Chat Page

--- src/app/chat/chat/chat.component.ts
+++ src/app/chat/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Message } from '../model/message';
 
 @Component({
   selector: 'app-chat',
@@ -11,22 +12,31 @@ export class ChatComponent implements OnInit {
 
   private client: Client;
   connected: Boolean = false;
+  message: Message = new Message();
+  messages: Message[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.client = new Client();
     this.client.webSocketFactory = () => {
       return new SockJS("http://localhost:8080/chat-websocket");
-    };
+    }
     this.client.onConnect = (frame) => {
       console.log('Connected: ' + this.client.connected + ' : ' + frame);
       this.connected = true;
+
+      this.client.subscribe('/chat/message', e => {
+        let message: Message = JSON.parse(e.body) as Message;
+        this.messages.push(message);
+        console.log(message);
+      });
     };
+
     this.client.onDisconnect = (frame) => {
       console.log('Disconnected: ' + !this.client.connected + ' : ' + frame);
       this.connected = false;
-    }
+    };
   }
   connect(){
     this.client.activate();
@@ -36,6 +46,12 @@ export class ChatComponent implements OnInit {
   disconnect(){
     this.client.deactivate();
     this.connected = false;
+  }
+
+  sendMessage(): void{
+    console.log(this.message);
+    this.client.publish({destination:'/app/message', body: JSON.stringify(this.message)});
+    this.message.text = '';
   }
 
 
